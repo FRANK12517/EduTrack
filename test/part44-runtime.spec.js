eaa7a7b50f09fdd6cb4bd9127e2f2538828ce331
FRANK12517 <2.21802254e+08+FRANK12517@users.noreply.github.com>
2026-08-23 12:11:10 +0000

    Part 44 activate staging validation gates

---
+++ test/part44-runtime.spec.js
@@ -0,0 +1,3 @@
+'use strict';
+const assert=require('assert'); const pkg=require('../package.json'); assert.strictEqual(pkg.engines.node,'22.x');
+if(process.env.EDUTRACK_STAGING_RUNTIME) assert.strictEqual(process.env.EDUTRACK_STAGING_RUNTIME,'22.x'); else console.log('Part 44 runtime: NOT_PROVEN (actual Node 22 staging runtime unavailable).');

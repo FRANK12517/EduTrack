79f81a6c1373486935ffb228a464f4fb494ef110
Frank Abban <acquahk50@gmail.com>
2026-09-16 16:51:52 +0000

    Revert "ci(tidb): report failing migration statement"
    This reverts commit d366ac9f7bf7057e5f06973a6a7ff1959e9c36b9.

--- db/relational.js
+++ db/relational.js
@@ -140,11 +140,6 @@ async function migrate() {
   requireConfigured();
   const db = getPool();
   const conn = await db.getConnection();
-  const query = conn.query.bind(conn);
-  conn.query = async (statement, ...params) => {
-    try { return await query(statement, ...params); }
-    catch (error) { error.edutrackStatement = statement; throw error; }
-  };
   try {
     await conn.beginTransaction();
     await conn.query(TABLES[0]);
